2e93c2224094ce9f43b0c5563a5f829854319c0c
amine <amine.sghir@visian.fr>
2020-06-22 11:12:03 +0100

    serve media files

--- randomlunch/settings.py
+++ randomlunch/settings.py
@@ -146,5 +146,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static')
+]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
